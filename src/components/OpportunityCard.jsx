adfeb0df99e42db73dfd60ab76caf395faf8333a
info@osvitytech.com <sofiyakhanamjms@gmail.com>
2025-03-18 13:07:20 +0530

    Handling the sample data added the gradient for footer

--- src/components/OpportunityCard.jsx
+++ src/components/OpportunityCard.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
-import Img1 from "../assets/OpportunityImg1.png";
-import Img2 from "../assets/OpportunityImg2.png";
-import Img3 from "../assets/OpportunityImg3.png"
-import Img4 from "../assets/OpportunityImg4.png"
 
-
-const opportunities = [
-  { title: 'Quizzes', color: '#D1E9F3', img: Img1 },
-  { title: 'Hackthons', color: '#D4F3D1', img: Img2 },
-  { title: 'Scholarship', color: '#EAD1F3', img: Img3 },
-  { title: 'College Festival', color: '#F3EFD1', img: Img4 },
-];
+import { opportunities } from '../data/sample';
 
 const OpportunityCard = () => {
+
   return (
     <div style={{ textAlign: 'center', padding: '20px' }}>
       <Typography variant="h4" gutterBottom style={{ fontWeight: 600, fontSize: '46px' }}>
---
+++ src/data/sample.js
@@ -0,0 +1,10 @@
+import Img1 from "../assets/OpportunityImg1.png";
+import Img2 from "../assets/OpportunityImg2.png";
+import Img3 from "../assets/OpportunityImg3.png"
+import Img4 from "../assets/OpportunityImg4.png"
+export const opportunities = [
+  { title: 'Quizzes', color: '#D1E9F3', img: Img1 },
+  { title: 'Hackthons', color: '#D4F3D1', img: Img2 },
+  { title: 'Scholarship', color: '#EAD1F3', img: Img3 },
+  { title: 'College Festival', color: '#F3EFD1', img: Img4 },
+];
