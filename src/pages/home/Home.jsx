173f73ebe9dd3128bfeb32c60da71c26cea89771
info@osvitytech.com <sofiyakhanamjms@gmail.com>
2025-03-24 12:38:40 +0530

    Initial Commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,96 +1,12 @@
-import React from "react";
-import Navbar from "../../components/Navbar";
-import SearchBar from "../../components/SearchBar";
-import { scaleWidth } from "../../helpers/Scalefactor";
-import UsingUnstop from "../../components/usingUnstop";
-import CategoryCards from "../../components/CategoryCards";
-import ProfileList from "../../components/ProfileCards";
-import CourseCard from "../../components/CourseCard";
-import OpportunityCard from "../../components/OpportunityCard";
-import UpdatedCard from "../../components/UpdatedCard";
-import Footer from "../../components/Footer";
+import React from 'react'
+import Card from '../../components/Card'
 
 const Home = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        gap: `${scaleWidth(20)}px`,
-        justifyContent: "center",
-        position: "relative",
-        background:
-          "linear-gradient(to top, transparent 50%, transparent 80%, #7a5feb 97%, #ad9ff3 100%)",
-      }}
-    >
-      <Navbar />
-      <SearchBar />
-      <CategoryCards />
-      <ProfileList />
-      <UsingUnstop />
-      <CourseCard />
-      <OpportunityCard />
-      <UpdatedCard />
-      <Footer />
-
-      <div
-        style={{
-          position: "absolute",
-          left: "-3%",
-          bottom: "0px",
-          width: "220px",
-          height: "150px",
-          borderRadius: "70%",
-          background: "#7a5feb",
-          filter: "blur(45px)",
-          opacity: "1",
-          zIndex: 1,
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          left: "20%",
-          bottom: "0px",
-          width: "220px",
-          height: "150px",
-          borderRadius: "70%",
-          background: "#7a5feb",
-          filter: "blur(45px)",
-          opacity: "1",
-          zIndex: 1,
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          right: "20%",
-          bottom: "0px",
-          width: "220px",
-          height: "150px",
-          borderRadius: "70%",
-          background: "#7a5feb",
-          filter: "blur(45px)",
-          opacity: "1",
-          zIndex: 1,
-        }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          right: "3%",
-          bottom: "0px",
-          width: "220px",
-          height: "150px",
-          borderRadius: "70%",
-          background: "#7a5feb",
-          filter: "blur(45px)",
-          opacity: "1",
-          zIndex: 1,
-        }}
-      />
+    <div>
+      <Card/>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
