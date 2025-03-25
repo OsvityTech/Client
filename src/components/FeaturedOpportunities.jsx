76c55be698b2bf03f3dca849e3ee6703c34d7c1c
madhuri261 <madhurinandipalli@gmail.com>
2025-03-25 23:13:54 +0530

    created responsive screen

--- src/components/FeaturedOpportunities.jsx
+++ src/components/FeaturedOpportunities.jsx
@@ -2,83 +2,96 @@ import React from "react";
 import OpportunityImg5 from "../assets/OpportunityImg5.png";
 import Codenova from "../assets/Codenova.png";
 import Medicalcoding from "../assets/Medicalcoding.png";
+import flipkartrunwayimg from "../assets/flipkartrunwayimg.png";
+import salesimg from "../assets/salesimg.png";
+import smartIndiaImg from "../assets/smartIndiaImg.png";
+import salesInternshipImg from "../assets/salesInternshipImg.png";
+import TcsCodeVitaImg from "../assets/TcsCodeVitaImg.png";
+import codersHackathonImg from "../assets/codersHackathonImg.png";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import OpportunityCard from "./OpportunityCard";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-
-
-
-
 const opportunities = [
-  { title: "Coding Ninja’s 2025", image: OpportunityImg5, author: "Mindeniousss" },
+  { title: "Coding Ninja's 2025", image: OpportunityImg5, author: "Mindeniousss" },
   { title: "Code Nova for Kids", image: Codenova, author: "Mindeniousss" },
   { title: "Medical coding jobs", image: Medicalcoding, author: "Mindeniousss" },
-  { title: "Flipkart Runway S6", image: OpportunityImg5, author: "Mindeniousss" },
-  { title: "Sales Internship", image: OpportunityImg5, author: "Mindeniousss" },
-  { title: "Smart India Hackathon", image: OpportunityImg5, author: "Mindeniousss" },
-  { title: "Sales Internship", image: OpportunityImg5, author: "Mindeniousss" },
-  { title: "TCS Code Vita 2025", image: OpportunityImg5, author: "Mindeniousss" },
-  { title: "Coders Hackathon", image: OpportunityImg5, author: "Mindeniousss" },
+  { title: "Flipkart Runway S6", image: flipkartrunwayimg, author: "Mindeniousss" },
+  { title: "Sales Internship", image: salesimg, author: "Mindeniousss" },
+  { title: "Smart India Hackathon", image: smartIndiaImg, author: "Online" },
+  { title: "Sales Internship", image: salesInternshipImg, author: "Mindeniousss" },
+  { title: "TCS Code Vita 2025", image: TcsCodeVitaImg, author: "Online" },
+  { title: "Coders Hackathon", image: codersHackathonImg, author: "Online" },
 ];
 
-
-const settings = {
-  infinite: true,
-  speed: 1000,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows: false,
-  draggable: true,
-  swipe: true,
-  touchThreshold: 10,
-  pauseOnHover: false,
-};
-
-
-
 const FeaturedOpportunities = () => {
+
+  // Responsive settings for slider
+  const settings = {
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3, // Default for desktop
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    draggable: true,
+    swipe: true,
+    touchThreshold: 10,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 744, // Tablet View
+        settings: {
+          slidesToShow: 2, // Show 2 cards in tablet view
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768, // Mobile View
+        settings: {
+          slidesToShow: 1, // Show 1 card in mobile view
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="relative w-[1534px] h-[774px] top-[3399] left-[1px] bg-white-600 flex justify-center items-center mt-[3399px]">
-      <div className="absolute w-[554px] h-[80px] top-[67px] left-[445px] ">
-        <h1
-          className="absolute w-[411px] h-[42px] top-[37px] left-[123px] font-Roboto font-normal text-[35px] leading-[100%] tracking-[0%] text-[#1C4A81]"
-        >
+
+
+    <div className="relative w-full max-w-[1334px] mx-auto h-auto min-h-[774px] bg-white-600 flex flex-col justify-center items-center  overflow-hidden">
+
+
+
+      {/* Header section */}
+      <div className="w-full max-w-[554px] h-auto text-center mb-10 md:mb-16">
+        <h1 className="font-Roboto font-normal text-[28px] md:text-[35px] leading-[100%] tracking-[0%] text-[#1C4A81] mb-4">
           Featured Opportunities
         </h1>
-
-
-        <p className="absolute w-[554px] h-[21px] top-[96px] left-[37px] font-poppins font-light text-[16px] leading-[100%] tracking-[0%] text-black">
+        <p className="font-poppins font-light text-[14px] md:text-[16px] leading-[100%] tracking-[0%] text-black">
           Check out the curated opportunities handpicked for you from top organizations.
         </p>
       </div>
-      <div className="w-[80%] mx-auto ">
+
+      {/* Slider section */}
+      <div className="w-full max-w-[90%] mx-auto ">
         <Slider {...settings}>
           {opportunities.map((opportunity, index) => (
-            <div key={index} className="px-4">
+            <div key={index} className="px-2 md:px-4">
               <OpportunityCard {...opportunity} />
             </div>
           ))}
         </Slider>
       </div>
 
-
-
-
-      <div className="absolute top-[717px] left-[620px] w-[256px] h-[24px] text-[20px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-[#164A98] cursor-pointer flex items-center justify-center">
+      {/* Explore All button */}
+      <div className="mt-8 md:mt-12 w-full max-w-[256px] h-[24px] text-[18px] md:text-[20px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-[#164A98] cursor-pointer flex items-center justify-center">
         <span>Explore All Opportunities</span>
         <KeyboardArrowRightIcon className="text-black-600" fontSize="medium" />
       </div>
-
-
-
-
-
-
     </div>
   );
 };
--- src/components/OpportunityCard.jsx
+++ src/components/OpportunityCard.jsx
@@ -1,45 +1,50 @@
 import React from "react";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-
-
 const OpportunityCard = ({ title, image, author }) => {
   return (
-
-
-    <div className="w-[393px] h-[400px] bg-white rounded-3xl mt-[200px] ml-[100px] border border-[#164A98] - 400 relative top-[5px] left-[1px] !mt-[140px]">
+    <div className=" h-[400px] bg-white  relative  !mt-[30px] ml-[-10px] overflow-hidden">
+      {/* SVG Background Shape */}
+      <svg
+        width="650px"
+        height="410px"
+        viewBox="0 0 650 410"
+        fill="none"
+        stroke="#164A98"
+        xmlns="http://www.w3.org/2000/svg"
+        className="absolute top-0 left-0"
+      >
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M30 1.5C15 1.5 1.5 15 1.5 30V370C1.5 385 15 398.5 30 398.5H250C265 398.5 278.5 385 278.5 370V350C278.5 335 290 323.5 305 323.5H325C340 323.5 353.5 310 353.5 295V30C353.5 15 340 1.5 325 1.5H30ZM0.5 30C0.5 14 14 0.5 30 0.5H325C341 0.5 354.5 14 354.5 30V295C354.5 311 341 324.5 325 324.5H305C290 324.5 279.5 335 279.5 350V370C279.5 386 266 399.5 250 399.5H30C14 399.5 0.5 386 0.5 370V30Z"
+          fill="black"
+        />
+      </svg>
 
 
       {/* Image */}
-
-      <img src={image} alt={title} className="w-[363px] h-[282px] border border-gray-300 flex justify-center items-center rounded-[20px] overflow-hidden relative top-[18px] left-[16px]" />
+      <img src={image} alt={title} className="w-[330px] h-[262px] border border-gray-300 flex justify-center items-center rounded-[20px] overflow-hidden relative top-[18px] left-[12px]" />
 
       {/* Title & Author */}
-      <div className="text-left w-full mt-3 px-2">
-
-        <h3 className="absolute top-[317px] left-[16px] w-[237px] h-[36px] text-[24px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-gray-800">
+      <div className="text-left w-full ">
+        <h3 className="absolute top-[307px] left-[16px] w-[237px] h-[36px] text-[24px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-gray-800">
           {title}
         </h3>
-
-        <p className="absolute top-[363px] left-[16px] w-[109px] h-[24px] text-[16px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-gray-600">
+        <p className="absolute top-[360px] left-[16px] w-[109px] h-[24px] text-[18px] font-[400] leading-[100%] tracking-[0%] font-[Poppins] text-gray-600">
           {author}
         </p>
-
       </div>
 
-
       {/* Arrow Icon */}
-      <div className="absolute top-[325px] left-[300px] w-[80px] h-[80px] flex justify-end pr-2">
-        <div className="w-[65px] h-[65px] border border-gray-700 rounded-full flex justify-center items-center  hover:bg-[#164A98] hover:border-[#164A98]">
+      <div className="absolute top-[335px] left-[280px] w-[80px] h-[80px] flex justify-end pr-2">
+        <div className="w-[65px] h-[65px] border border-[#164A98] rounded-full flex justify-center items-center hover:bg-[#164A98] hover:border-[#164A98]">
           <ArrowForwardIosIcon
-            className="text-gray-500 transition duration-300 ease-in-out hover:text-white"
+            className="text-[#164A98] transition duration-300 ease-in-out hover:text-white"
             style={{ fontSize: "42px" }}
           />
         </div>
       </div>
-
-
-
     </div>
   );
 };
