2e1edcd7070cb246cc5da0909045beb0c33b3c9a
RohithBabuKondepu <rohithbabukondepu@gmail.com>
2025-03-27 02:32:09 +0530

    merges

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,56 @@
-<<<<<<< HEAD
+// // <<<<<<< HEAD
+// import Home from "./pages/home/Home";
+// import Jobs from "../src/pages/jobs.jsx"
+// const App = () => {
+//   return( 
+//     <div>
+//     <div class=" bg-[linear-gradient(to_bottom,_#144D9E_0%,_#61B7E6_15%,_#FFFFFF_30%,_#FFFFFF_100%)] lg:px-25 lg:py-8 sm:px-10   sm:py-6 px-8 py-4 ">
+//       <Home />
+// =======
+
+
+// function App() {
+//   return (
+//     <div className='w-screen'>
+//       <Home/> 
+// {/* >>>>>>> a49ee01169ec8c3052589b51aa707b03264c1c95 */}
+//     </div>
+//     <div className="bg-gradient-to-b from-white to-purple-100 h-[60vh] p-10 rounded-lg w-full  "> 
+//     <Jobs/>
+//     </div>
+    
+//     </div>
+  
+//   );
+// };
+// export default App;
+
+
+
 import Home from "./pages/home/Home";
 import Jobs from "../src/pages/jobs.jsx"
+import FooterPage from "../src/components/Sections/FooterSection.jsx"
+import StayUpdate from "../src/components/Sections/StayUpdateSection.jsx";
+import MentorSection from "../src/components/Sections/MentorSection.jsx";
+import OpportunitySection from "../src/components/Sections/OpportunitySection.jsx"
+
 const App = () => {
   return( 
     <div>
-    <div class=" bg-[linear-gradient(to_bottom,_#144D9E_0%,_#61B7E6_15%,_#FFFFFF_30%,_#FFFFFF_100%)] lg:px-25 lg:py-8 sm:px-10   sm:py-6 px-8 py-4 ">
+    <div class=" bg-[linear-gradient(to_bottom,_#144D9E_0%,_#61B7E6_15%,_#FFFFFF_50%,_#FFFFFF_100%)] lg:px-30 lg:py-8 sm:px-10 sm:py-6 px-8 py-4 ">
       <Home />
-=======
-import Home from './pages/home/Home';
-
-function App() {
-  return (
-    <div className='w-screen'>
-      <Home/> 
->>>>>>> a49ee01169ec8c3052589b51aa707b03264c1c95
     </div>
     <div className="bg-gradient-to-b from-white to-purple-100 h-[60vh] p-10 rounded-lg w-full  "> 
     <Jobs/>
     </div>
-    
+    <OpportunitySection />
+    <div className='bg-[#FFFFFF]'>
+      <MentorSection />
+      </div>
+    <StayUpdate />
+    <FooterPage />
     </div>
-  
+    
   );
 };
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -28,16 +28,12 @@
 import React from "react";
 import Navbar from "../../components/Navbar";
 import HeroSection from "../../components/HeroSection";
-import MentorSection from "../../components/Sections/MentorSection";
 
 const Home = () => {
   return (
     <div >
       <Navbar />
       <HeroSection />
-      <div className='bg-[#FFFFFF]'>
-      <MentorSection />
-      </div>
     </div>
     
   );
@@ -45,23 +41,4 @@ const Home = () => {
 
 export default Home;
 
-=======
-import React from "react";
-import MentorSection from "../../components/Sections/MentorSection";
-import OpportunitySection from "../../components/Sections/OpportunitySection";
-import StayUpdate from "../../components/Sections/StayUpdateSection";
-import FooterPage from "../../components/Sections/FooterSection";
 
-const Home = () => {
-  return (
-    <div className="bg-[#FFFFFF] w-full">
-      <OpportunitySection />
-      <MentorSection />
-      <StayUpdate />
-      <FooterPage />
-    </div>
-  );
-};
-
-export default Home;
->>>>>>> a49ee01169ec8c3052589b51aa707b03264c1c95
